c0cf1a7b5db74d5a76015b7e3e95ee81727772bc
berry <yfy465@outlook.com>
2021-06-14 11:30:28 +0800

    完善URL组

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import json5
 weatherUrl = ['https://api.caiyunapp.com/v2.5/']
 weatherjson = [{},{},{},{},{},{},{},{},{}]
 haveUnitExcept = False
-UnitExcept = []
-
+UnitExcept = {}
 
 def getlocation(jsonobject):#按参数jsonobject中指定方式获取经纬度
 	#TODO：读取csv定位、输入经纬度、使用设备定位
@@ -19,7 +18,7 @@ def getlocation(jsonobject):#按参数jsonobject中指定方式获取经纬度
 
 	return '119.166485,36.654448'#待todo完成后会返回定位的经纬度，现在这个是山东省潍坊市坊子区的位置
 
-def settingload() :#读取settings.json并创建请求URL组
+def settingload():#读取settings.json并创建请求URL组
 	global weatherUrl
 	global haveUnitExcept
 	global UnitExcept
@@ -40,16 +39,30 @@ def settingload() :#读取settings.json并创建请求URL组
 	if unitexcept != {} :
 		weatherUrl[0] = weatherUrl[0]+token+'/'+location+'/weather.json?unit='+unit
 		haveUnitExcept = True
+
+		# 创建请求URL组
+		i = 1
 		if 'SI' in unitexcept.values():
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=SI')
+			UnitExcept['SI'] = i
+			i+=1
 		if 'imperial' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=imperial')
+			UnitExcept['imperial'] = i
+			i+=1
 		if 'metric:v1' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=metric:v1')
+			UnitExcept['metric:v1'] = i
+			i+=1
 		if 'metric:v2' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=metric:v2')
+			UnitExcept['metric:v2'] = i
+			i+=1
 		if 'default metric' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json')
+			UnitExcept['default metric'] = i
+			i+=1
+		del(i)
 
 	else:
 		if unit == 'default metric' :
@@ -58,7 +71,6 @@ def settingload() :#读取settings.json并创建请求URL组
 			weatherUrl[0] = weatherUrl[0]+token+'/'+location+'/weather.json?unit='+unit
 
 	setfile.close()
-
 
 def getWeather():#请求天气信息
 	global weatherjson
@@ -72,7 +84,11 @@ def getWeather():#请求天气信息
 		i+=1
 	del(i)
 
-def typer() :#TODO 获取信息并对获取的信息进行格式化
+def digitalPrint(num):
+	oneUpper = "'|"
+	oneDowner = ''
+
+def typer():#TODO 获取信息并对获取的信息进行格式化
 	getWeather()
 	
 	res = ''
@@ -83,8 +99,6 @@ def typer() :#TODO 获取信息并对获取的信息进行格式化
 #######################################################################################
 
 
-#判断是否成功获取天气并输出 TODO：在这里判断是否成功并不合理，待重构
-# if (weatherjson['status'] == 'ok') :   
-# 	print(typer())
-# else:
-# 	print("ERROR!\n"+weatherjson['error'])
+#输出
+print(typer())
+print()
