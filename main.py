fbdc0943903946d1b5746ab969dc13470eed5ecf
berry <yfy465@outlook.com>
2021-06-14 11:00:13 +0800

    Done:url组创建与天气获取；ToDo：位置获取、数据格式化输出

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import urllib.request as urlreq
-import json
+import json5
 
-weatherUrl = ''
-weatherjson = {}
+weatherUrl = ['https://api.caiyunapp.com/v2.5/']
+weatherjson = [{},{},{},{},{},{},{},{},{}]
+haveUnitExcept = False
+UnitExcept = []
 
 
 def getlocation(jsonobject):#按参数jsonobject中指定方式获取经纬度
@@ -13,13 +15,18 @@ def getlocation(jsonobject):#按参数jsonobject中指定方式获取经纬度
 			
 	# except KeyError:
 	# 	print('请检查settings.json是否正确（可前往Github repo下载默认值）')
+	# 	exit(-1)
 
 	return '119.166485,36.654448'#待todo完成后会返回定位的经纬度，现在这个是山东省潍坊市坊子区的位置
 
-def settingload() :#读取settings.json并拼接请求URL
+def settingload() :#读取settings.json并创建请求URL组
+	global weatherUrl
+	global haveUnitExcept
+	global UnitExcept
+
 	#文件读取
-	setfile = open('.\\settings.json',mode='r')
-	setjson = json.loads(setfile.read())
+	setfile = open('.\\settings.json',mode='r',encoding='utf-8')
+	setjson = json5.loads(setfile.read())
 
 	try:
 		token = setjson['token']
@@ -28,26 +35,56 @@ def settingload() :#读取settings.json并拼接请求URL
 		unitexcept = setjson['unitexcept']
 	except KeyError:
 		print('请检查settings.json是否正确（可前往Github repo下载默认值）')
+		exit(-1)
 
-	
+	if unitexcept != {} :
+		weatherUrl[0] = weatherUrl[0]+token+'/'+location+'/weather.json?unit='+unit
+		haveUnitExcept = True
+		if 'SI' in unitexcept.values():
+			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=SI')
+		if 'imperial' in unitexcept.values() :
+			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=imperial')
+		if 'metric:v1' in unitexcept.values() :
+			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=metric:v1')
+		if 'metric:v2' in unitexcept.values() :
+			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=metric:v2')
+		if 'default metric' in unitexcept.values() :
+			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json')
 
+	else:
+		if unit == 'default metric' :
+			weatherUrl[0] = weatherUrl[0]+token+'/'+location+'/weather.json'
+		else:
+			weatherUrl[0] = weatherUrl[0]+token+'/'+location+'/weather.json?unit='+unit
+
+	setfile.close()
 
 
 def getWeather():#请求天气信息
 	global weatherjson
-	weatherUrlRespond = urlreq.urlopen(weatherUrl)
-	weatherjson= json.loads(weatherUrlRespond.read())
 
-def typer() :#对获取的信息进行格式化
+	settingload()
+
+	i=0
+	for thisURL in weatherUrl:
+		weatherUrlRespond = urlreq.urlopen(thisURL)
+		weatherjson[i]= json5.loads(weatherUrlRespond.read())
+		i+=1
+	del(i)
+
+def typer() :#TODO 获取信息并对获取的信息进行格式化
+	getWeather()
+	
 	res = ''
 	
+	#return res
 	return weatherjson
 
 #######################################################################################
 
 
 #判断是否成功获取天气并输出 TODO：在这里判断是否成功并不合理，待重构
-if (weatherjson['status'] == 'ok') :   
-	print(typer())
-else:
-	print("ERROR!\n"+weatherjson['error'])
+# if (weatherjson['status'] == 'ok') :   
+# 	print(typer())
+# else:
+# 	print("ERROR!\n"+weatherjson['error'])
