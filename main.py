4638a74ccd880d4682765341e2e5df5139ff202a
berry <yfy465@outlook.com>
2021-06-15 10:55:54 +0800

    暂存

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import json5
 weatherUrl = ['https://api.caiyunapp.com/v2.5/']
 weatherjson = [{},{},{},{},{},{},{},{},{}]
 haveUnitExcept = False
-UnitExcept = {}
+unit = ''
+unitexcept = {}
+UnitExceptSort = {}
+unitSymbol={'temp':{'SI':'K','imperial':'℉','metric:v1':'℃','metric:v2':'℃','default metric':'℃'}}
+skycon = {}
 
 def getlocation(jsonobject):#按参数jsonobject中指定方式获取经纬度
 	#TODO：读取csv定位、输入经纬度、使用设备定位
@@ -21,7 +25,10 @@ def getlocation(jsonobject):#按参数jsonobject中指定方式获取经纬度
 def settingload():#读取settings.json并创建请求URL组
 	global weatherUrl
 	global haveUnitExcept
-	global UnitExcept
+	global UnitExceptSort
+	global unitexcept
+	global unit
+	global skycon
 
 	#文件读取
 	setfile = open('.\\settings.json',mode='r',encoding='utf-8')
@@ -32,6 +39,7 @@ def settingload():#读取settings.json并创建请求URL组
 		location = getlocation(setjson)
 		unit = setjson['unit']
 		unitexcept = setjson['unitexcept']
+		skycon = setjson['skycon']
 	except KeyError:
 		print('请检查settings.json是否正确（可前往Github repo下载默认值）')
 		exit(-1)
@@ -44,23 +52,23 @@ def settingload():#读取settings.json并创建请求URL组
 		i = 1
 		if 'SI' in unitexcept.values():
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=SI')
-			UnitExcept['SI'] = i
+			UnitExceptSort['SI'] = i
 			i+=1
 		if 'imperial' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=imperial')
-			UnitExcept['imperial'] = i
+			UnitExceptSort['imperial'] = i
 			i+=1
 		if 'metric:v1' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=metric:v1')
-			UnitExcept['metric:v1'] = i
+			UnitExceptSort['metric:v1'] = i
 			i+=1
 		if 'metric:v2' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json?unit=metric:v2')
-			UnitExcept['metric:v2'] = i
+			UnitExceptSort['metric:v2'] = i
 			i+=1
 		if 'default metric' in unitexcept.values() :
 			weatherUrl.append('https://api.caiyunapp.com/v2.5/'+token+'/'+location+'/weather.json')
-			UnitExcept['default metric'] = i
+			UnitExceptSort['default metric'] = i
 			i+=1
 		del(i)
 
@@ -84,17 +92,50 @@ def getWeather():#请求天气信息
 		i+=1
 	del(i)
 
-def digitalPrint(num):
+def digitalPrint(num):# TODO：显示字符画数字 
 	oneUpper = "'|"
 	oneDowner = ''
 
 def typer():#TODO 获取信息并对获取的信息进行格式化
 	getWeather()
 	
-	res = ''
+	res = '\n\t'
+	if haveUnitExcept == False:
+		res+=str(weatherjson[0]['result']['realtime']['temperature'])+unitSymbol['temp'][unit]
+		res+='\n\t'
+		res+=skycon[weatherjson[0]['result']['realtime']['skycon']]
+		res+='\n\t\t'
+		res+='(体感温度：'+str(weatherjson[0]['result']['realtime']['apparent_temperature'])+unitSymbol['temp'][unit]+' )'
+		res+='\n'
+
+		res+=weatherjson[0]['result']['hourly']['description']
+
+	elif 'precipitation' in unitexcept:
+		res+=str(weatherjson[0]['result']['realtime']['temperature'])+unitSymbol['temp'][unit]
+		res+='\n\t'
+		res+=skycon[weatherjson[0]['result']['realtime']['skycon']]
+		res+='\n\t\t'
+		res+='(体感温度：'+str(weatherjson[0]['result']['realtime']['apparent_temperature'])+unitSymbol['temp'][unit]+' )'
+		res+='\n'
+
+		res+=weatherjson[0]['result']['hourly']['description']
+
 	
-	#return res
-	return weatherjson
+
+	elif 'temp' in unitexcept:
+		res+=str(weatherjson[UnitExceptSort[unitexcept['temp']]]['result']['realtime']['temperature'])+unitSymbol['temp'][unitexcept['temp']]
+		res+='\n\t'
+		res+=skycon[weatherjson[0]['result']['realtime']['skycon']]
+		res+='\n\t\t'
+		res+='(体感温度：'+str(weatherjson[UnitExceptSort[unitexcept['temp']]]['result']['realtime']['apparent_temperature'])+unitSymbol['temp'][unitexcept['temp']]+' )'
+		res+='\n'
+
+		res+=weatherjson[0]['result']['hourly']['description']
+	
+		
+		
+	return res
+	#return weatherjson
 
 #######################################################################################
 
